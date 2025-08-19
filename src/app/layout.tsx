2a078ad0ea954a0159c1fddc94c9d10eaeaba336
Niclas Bångman <niclasbangman@gmail.com>
2025-08-19 17:00:07 +0200

    fix: rm unused mono font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, League_Spartan } from "next/font/google";
+import { Geist, League_Spartan } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -40,9 +35,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${titleFont} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${titleFont} antialiased`}>
         {children}
       </body>
     </html>
